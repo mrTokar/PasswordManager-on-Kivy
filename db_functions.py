648198f598dac779b8b62b7de2a873ae91cd6380
Tokar <egortok2014@gmail.com>
2022-02-23 00:02:29 +0300

    Add delete_note functions. In Bd classes add functoins for delete info to key (Commit on 'master')

--- db_functions.py
+++ db_functions.py
@@ -2,6 +2,7 @@ import os
 import sqlite3
 
 class DB:
+
     def __init__(self):
         if os.path.isdir('sources'): 
             "Если в данной дирректории есть папка sources"
@@ -17,11 +18,13 @@ class DB:
 
     def connect_db(self):
         "Подключение к БД"
+
         self.connection = sqlite3.connect("sources\\data.db", check_same_thread=False)
         self.cursor = self.connection.cursor()
 
     def connection_close(self):
         "Отключение от БД"
+
         if self.cursor is not None:
             self.cursor.close()
         if self.connection is not None:
@@ -29,14 +32,16 @@ class DB:
     
     def check_connection(self) -> bool:
         "Проверка соединения"
+
         if self.cursor is not None:
             if self.connection is not None:
                 return True
 
     def load(self, login: str, key: str) -> list:
-        """Загрузка информации из бд по ключу.
-        Возвращает список [name, nickname, password, icon]
-        login - нужная талица в БД."""
+        """Загрузка информации из бд по ключу key.
+        Возвращает список [name, nickname, password, icon] \n
+        login - нужная талица в БД \n
+        key - ключ, по каторому нужно загрузить информацию"""
                
         self.cursor.execute('SELECT * FROM {}'.format(login))
         rows = self.cursor.fetchall()
@@ -46,16 +51,18 @@ class DB:
                 return list(map(str, note))
 
     def load_all_name(self, login: str) -> list:
-        """Возвращает все записи из таблицы нужной таблицы.
-        login - нужная таблица в БД"""
+        """Возвращает все записи из таблицы нужной таблицы. \n
+        login - нужная таблица в БД \n"""
+
         self.cursor.execute('SELECT * FROM {}'.format(login))
         rows = self.cursor.fetchall()
         return [note[0] for note in rows]
 
     def save(self, login: str, arr: list):
         """Сохранение списка arr [name, nickname, password, icon].
-        Каждый эдлемент соответственно в свое поле БД.
-        login - нужная табдица в БД"""
+        Каждый эдлемент соответственно в свое поле БД. \n
+        login - нужная табдица в БД \n
+        arr - сохраняемый список"""
         
         info_cells = list(self.cursor.execute("PRAGMA table_info({});".format(login)))
         keys =[]
@@ -71,7 +78,9 @@ class DB:
         self.connection.commit()
 
     def create_new_table(self, login: str):
-        """Создание новой таблицы в БД"""
+        """Создание новой таблицы в БД \n
+        login - название новой таблицы"""
+
         if self.check_connection():
 
             len_cells = 4
@@ -99,7 +108,17 @@ class DB:
             self.create_new_table(login)
 
 
+    def delete_note(self, login: str, key: str):
+        """Удаление строчки по заначению key в стобце name из таблицы login в БД. \n
+        login - нужная табkbwf в БД \n
+        key - значение в ячейке name (ключ по котрой будет удалена строчка)"""
+
+        self.cursor.execute("DELETE FROM {0} WHERE {1} = '{2}'".format(login, "name", key))
+        self.connection.commit()
+
+
 class DB_hash:
+
     def __init__(self):
         if os.path.isdir('sources'): 
             "Если в данной дирректории есть папка sources"
@@ -116,11 +135,13 @@ class DB_hash:
 
     def connect_db(self):
         "Подключение к БД"
+
         self.connection = sqlite3.connect("sources\\hashedpasswords.db", check_same_thread=False)
         self.cursor = self.connection.cursor()
 
     def connection_close(self):
         "Отключение от БД"
+
         if self.cursor is not None:
             self.cursor.close()
         if self.connection is not None:
@@ -128,28 +149,33 @@ class DB_hash:
     
     def check_connection(self) -> bool:
         "Проверка соединения"
+
         if self.cursor is not None:
             if self.connection is not None:
                 return True
 
     def saving(self, login: str, key: bytes, salt: bytes):
-        "Cохраняет ключ и соль для пользоваетля"
+        """Cохраняет ключ и соль для пользоваетля \n
+        login - нужный пользователь \n
+        key - сохраняемый ключ \n
+        salt - сохраняемая соль"""
+
         info_cells = list(self.cursor.execute("PRAGMA table_info(hash);"))
-            
         keys =[]
         for i in info_cells:
             keys.append(i[1])
         keys = ", ".join(keys)
 
-        
         vaules = f"\"{login}\", \"{str(key)}\", \"{str(salt)}\""
         
         operation = (f'INSERT or REPLACE into hash ({keys}) VALUES ({vaules})')
         self.cursor.execute(operation)
         self.connection.commit()
 
-    def load(self, login:str) -> tuple or None:
-        "Возвращает (key, salt) из БД. Если записи не существует возвращает None" 
+    def load(self, login: str) -> tuple or None:
+        """Возвращает (key, salt) из БД. Если записи не существует возвращает None \n
+        login - нужный пользователь, по котрому нужно загрузить кортеж"""
+
         self.cursor.execute('SELECT * FROM hash')
         rows = self.cursor.fetchall()
         if rows is None:
@@ -160,6 +186,7 @@ class DB_hash:
 
     def create_new_table(self):
         """Создание таблицы в БД (в случае ее отсутствия)"""
+
         if self.check_connection():
 
             len_cells = 3
@@ -184,12 +211,20 @@ class DB_hash:
 
     def check_table(self):
         """Проверка существования таблицы. В случае отсутвия создает ее."""
+
         try:
             self.cursor.execute('SELECT * FROM hash')
         except sqlite3.OperationalError:
             self.create_new_table()
-
     
+
+    def delete_note(self, login: str):
+        """Удаление входных данных пользователя в БД. \n
+        login - нужный пользователь"""
+
+        self.cursor.execute("DELETE FROM {0} WHERE {1} = '{2}'".format('hash', 'name', login))
+        self.connection.commit()
+
 
 if __name__ == "__main__":
     test = DB()
