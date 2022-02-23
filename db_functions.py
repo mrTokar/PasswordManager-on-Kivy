38197c4ec3a8a8bbd9365485fdc253ebc91aa9a3
Tokar <egortok2014@gmail.com>
2022-02-23 11:45:47 +0300

    Table update

--- db_functions.py
+++ db_functions.py
@@ -3,7 +3,7 @@ import sqlite3
 
 class DB:
 
-    def __init__(self):
+    def __init__(self, table):
         if os.path.isdir('sources'): 
             "Если в данной дирректории есть папка sources"
 
@@ -15,6 +15,9 @@ class DB:
             file = open("sources\\data.db", "w+")
             file.close()
         self.connect_db()
+
+        self.table = table
+        self.check_table()
 
     def connect_db(self):
         "Подключение к БД"
@@ -37,34 +40,39 @@ class DB:
             if self.connection is not None:
                 return True
 
-    def load(self, login: str, key: str) -> list:
+    def check_table(self):
+        """Проверка существования таблицы. В случае отсутвия создает ее."""
+
+        try:
+            self.cursor.execute('SELECT * FROM {self.table}')
+        except sqlite3.OperationalError:
+            self.create_new_table()
+
+    def load(self, key: str) -> list:
         """Загрузка информации из бд по ключу key.
         Возвращает список [name, nickname, password, icon] \n
-        login - нужная талица в БД \n
         key - ключ, по каторому нужно загрузить информацию"""
                
-        self.cursor.execute('SELECT * FROM {}'.format(login))
+        self.cursor.execute('SELECT * FROM {}'.format(self.table))
         rows = self.cursor.fetchall()
 
         for note in rows:
             if note[0] == key:
                 return list(map(str, note))
 
-    def load_all_name(self, login: str) -> list:
-        """Возвращает все записи из таблицы нужной таблицы. \n
-        login - нужная таблица в БД \n"""
+    def load_all_name(self) -> list:
+        """Возвращает все записи из таблицы нужной таблицы."""
 
-        self.cursor.execute('SELECT * FROM {}'.format(login))
+        self.cursor.execute('SELECT * FROM {}'.format(self.table))
         rows = self.cursor.fetchall()
         return [note[0] for note in rows]
 
-    def save(self, login: str, arr: list):
+    def save(self, arr: list):
         """Сохранение списка arr [name, nickname, password, icon].
         Каждый эдлемент соответственно в свое поле БД. \n
-        login - нужная табдица в БД \n
         arr - сохраняемый список"""
         
-        info_cells = list(self.cursor.execute("PRAGMA table_info({});".format(login)))
+        info_cells = list(self.cursor.execute("PRAGMA table_info({});".format(self.table)))
         keys =[]
         for i in info_cells:
             keys.append(i[1])
@@ -73,16 +81,14 @@ class DB:
         string = lambda x: "'" + x + "'"
         vaules = ', '.join(list(map(string, arr)))
         
-        operation = (f'INSERT or REPLACE into {login} ({keys}) VALUES ({vaules})')
+        operation = (f'INSERT or REPLACE into {self.table} ({keys}) VALUES ({vaules})')
         self.cursor.execute(operation)
         self.connection.commit()
 
-    def create_new_table(self, login: str):
-        """Создание новой таблицы в БД \n
-        login - название новой таблицы"""
+    def create_new_table(self):
+        """Создание новой таблицы в БД"""
 
         if self.check_connection():
-
             len_cells = 4
             name_of_cells = ['name', 'nickname', 'password', 'icon']
             key = 0
@@ -97,23 +103,22 @@ class DB:
             cells = cells.rstrip(cells[-1])
             cells = cells.rstrip(cells[-1])
             try:
-                self.cursor.execute(f'CREATE TABLE {login}({cells})')
+                self.cursor.execute(f'CREATE TABLE {self.table}({cells})')
                 self.connection.commit()
-                self.save(login, ["Example", "test2020@gmail.com", "123456789", "default"])
+                self.save(self.table, ["Example", "test2020@gmail.com", "123456789", "default"])
             except sqlite3.OperationalError:
                 pass
 
         else:
             self.connect_db()
-            self.create_new_table(login)
+            self.create_new_table(self.table)
 
 
-    def delete_note(self, login: str, key: str):
+    def delete_note(self, key: str):
         """Удаление строчки по заначению key в стобце name из таблицы login в БД. \n
-        login - нужная табkbwf в БД \n
         key - значение в ячейке name (ключ по котрой будет удалена строчка)"""
 
-        self.cursor.execute("DELETE FROM {0} WHERE {1} = '{2}'".format(login, "name", key))
+        self.cursor.execute("DELETE FROM {0} WHERE {1} = '{2}'".format(self.table, "name", key))
         self.connection.commit()
 
     
@@ -235,11 +240,10 @@ class DB_hash:
 
 
 if __name__ == "__main__":
-    test = DB()
-    test.create_new_table("tokar")
-    test.save("tokar", ["steam", "kek", "123", "+"])
-    print(test.load("tokar", "steam"))
-    print(test.load_all_name('tokar'))
+    test = DB("tokar")
+    test.save(["steam", "kek", "123", "+"])
+    print(test.load("steam"))
+    print(test.load_all_name())
 
     test2 = DB_hash()
     test2.saving("tokar", bytes('10101100', encoding='utf-8'), bytes('100110110', encoding='utf-8'))
